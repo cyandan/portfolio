021e5ada49ba38a7f241b499797d5686f71bb311
Dan <daniel.alan.wilson@gmail.com>
2019-01-25 16:53:57 +0000

    Initial commit

---
+++ app.js
@@ -0,0 +1,15 @@
+// PACKAGE REQUIREMENTS
+var express = require("express");
+    
+// APP CONFIG
+var app = express();
+app.use(express.static(__dirname + "/public"));
+
+// ROUTE CONFIG
+var routes  = require("./routes/routes.js");
+app.use(routes);
+
+// REQUEST LISTENER
+app.listen(process.env.PORT, process.env.IP, function(){
+    console.log("Server started.");
+});
---
+++ routes/routes.js
@@ -0,0 +1,29 @@
+// PACKAGE REQUIREMENTS
+var express = require("express");
+    
+// ROUTER CONFIG
+var router  = express.Router();
+    
+// ROUTES
+router.get("/", function(req, res){
+    res.render("landing.ejs");
+});
+
+router.get("/company", function(req, res){
+    res.render("company.ejs");
+});
+
+router.get("/survey", function(req, res){
+    res.render("survey.ejs");
+});
+
+router.get("/technical", function(req, res){
+    res.render("technical.ejs");
+});
+
+router.get("*", function(req, res){
+    res.render("landing.ejs");
+});
+    
+// EXPORT ROUTES
+module.exports = router;
